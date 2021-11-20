f479a408c149c9ec7e724f6201ef6ab13361573d
Christian Tusset <christiantusset@gmail.com>
2021-11-20 19:10:37 -0300

    update scripts

--- scripts/install/general-pacman-packages.sh
+++ scripts/install/general-pacman-packages.sh
@@ -1,17 +1,44 @@
 #!/usr/bin/env bash
 
 echo "[START]: general-packages installation..."
+
+# Functions
+function _exit ( ) {
+  rm -f "$O" "$E"
+  exit "$1"
+}
+
+function _do ( ) {
+  "$@" 1>"$O" 2>"$E"
+  _check "$?"
+}
+
+function _check ( ) {
+  if [ "$1" -ne 0 ] ; then
+    echo '! NONZERO RETURN CODE'
+    echo ' > There was an error!'
+    >&1 cat "$O"
+    >&2 cat "$E"
+    _exit "$1"
+  fi
+}
+
+function _trap ( ) {
+  echo '! TRAP'
+  echo ' > There was an error!'
+  _exit 70;
+}
+
+# Update System
 yes | sudo pacman -Syu
 
-DOTFILES="$(dirname "$(dirname "${BASH_SOURCE[0]}")")";
+# Install Packages
+trap _trap SIGHUP SIGINT SIGTERM
 
+DOTFILES="$(dirname "$(dirname "${BASH_SOURCE[0]}")")";
 packages="$(sed -e 's/\s*#.*$//g' -e '/^$/d' "$DOTFILES"/scripts/packages/pacman)"
 
 echo "  > installing ~ $(<<< "$packages" tr '\n' ' ')"
 _do pacman -S --needed --noconfirm - <<< "$packages"
-
-# Fix recoll filters
-sed 's:^#!/usr/bin/env python$:#!/usr/bin/env python2:g' -i /usr/share/recoll/filters/*
-sed 's:^#!/usr/bin/python$:#!/usr/bin/env python2:g' -i /usr/share/recoll/filters/*
 
 echo "[FINISHED]: general-packages installation"
