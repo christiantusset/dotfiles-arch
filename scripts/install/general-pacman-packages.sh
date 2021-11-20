15c3140afda84cea5b72e752e6d0f6bde4e4fba1
Christian Tusset <christiantusset@gmail.com>
2021-11-20 19:04:33 -0300

    update scripts

--- scripts/install/general-pacman-packages.sh
+++ scripts/install/general-pacman-packages.sh
@@ -3,7 +3,9 @@
 echo "[START]: general-packages installation..."
 yes | sudo pacman -Syu
 
-packages="$(sed -e 's/\s*#.*$//g' -e '/^$/d' ~/Documents/dotfiles-arch/scripts/packages/pacman)"
+DOTFILES="$(dirname "$(dirname "${BASH_SOURCE[0]}")")";
+
+packages="$(sed -e 's/\s*#.*$//g' -e '/^$/d' "$DOTFILES"/scripts/packages/pacman)"
 
 echo "  > installing ~ $(<<< "$packages" tr '\n' ' ')"
 _do pacman -S --needed --noconfirm - <<< "$packages"
