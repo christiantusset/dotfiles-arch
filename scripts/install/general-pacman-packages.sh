8313b17fb1d07024ec818c4ebdfef2fd8a701abf
Christian Tusset <christiantusset@gmail.com>
2021-11-20 19:06:35 -0300

    update scripts

--- scripts/install/general-pacman-packages.sh
+++ scripts/install/general-pacman-packages.sh
@@ -10,4 +10,8 @@ packages="$(sed -e 's/\s*#.*$//g' -e '/^$/d' "$DOTFILES"/scripts/packages/pacman
 echo "  > installing ~ $(<<< "$packages" tr '\n' ' ')"
 _do pacman -S --needed --noconfirm - <<< "$packages"
 
+# Fix recoll filters
+sed 's:^#!/usr/bin/env python$:#!/usr/bin/env python2:g' -i /usr/share/recoll/filters/*
+sed 's:^#!/usr/bin/python$:#!/usr/bin/env python2:g' -i /usr/share/recoll/filters/*
+
 echo "[FINISHED]: general-packages installation"
