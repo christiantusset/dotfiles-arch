aaf5813b53806888339edb25164fdafe8a6f0587
Christian Tusset <christiantusset@gmail.com>
2021-11-20 19:58:03 -0300

    update scripts

--- scripts/install/general-pacman-packages.sh
+++ scripts/install/general-pacman-packages.sh
@@ -3,6 +3,37 @@
 # Start
 echo "[START]: general-packages installation..."
 
+# Temp Files
+O="$(mktemp --tmpdir "${USER}-install-arch-out.XXXXXXXX")"
+E="$(mktemp --tmpdir "${USER}-install-arch-err.XXXXXXXX")"
+
+# Functions
+function _exit ( ) {
+  rm -f "$O" "$E"
+  exit "$1"
+}
+
+function _do ( ) {
+  "$@" 1>"$O" 2>"$E"
+  _check "$?"
+}
+
+function _check ( ) {
+  if [ "$1" -ne 0 ] ; then
+    echo '! NONZERO RETURN CODE'
+    echo ' > There was an error!'
+    >&1 cat "$O"
+    >&2 cat "$E"
+    _exit "$1"
+  fi
+}
+
+function _trap ( ) {
+  echo '! TRAP'
+  echo ' > There was an error!'
+  _exit 70;
+}
+
 # Update System
 yes | sudo pacman -Syu
 
@@ -10,9 +41,8 @@ yes | sudo pacman -Syu
 packages="$(sed -e 's/\s*#.*$//g' -e '/^$/d' ./scripts/packages/pacman)"
 
 echo "  > installing ~ $(<<< "$packages" tr '\n' ' ')"
-printf '%s\n' "$packages" |
-1>/dev/null 2>&1 pacman -S --needed --noconfirm -
+_do pacman -S --needed --noconfirm - <<< "$packages"
 
 # Finish
 echo "[FINISHED]: general-packages installation"
-exit 0
+_exit 0
