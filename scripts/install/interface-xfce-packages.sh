686ec08a877f161d7c67c70e69b5f9ef87115caf
Christian Tusset <christiantusset@gmail.com>
2021-11-20 20:01:37 -0300

    update scripts

--- scripts/install/interface-xfce-packages.sh
+++ scripts/install/interface-xfce-packages.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
 
+# Start
 echo "[START]: xfce-packages installation..."
 
+# Update System
 yes | sudo pacman -Syu
-yes | sudo pacman -Sy --needed --overwrite "*" --nodeps --nodeps `cat ./scripts/packages/interfaces/xfce` || exit 1
 
-echo "[FINISHED]: xfce-packages installation"
+# Install XFCE Packages
+packages="$(sed -e 's/\s*#.*$//g' -e '/^$/d' ./scripts/packages/interfaces/xfce)"
+
+echo "  > installing ~ $(<<< "$packages" tr '\n' ' ')"
+printf '%s\n' "$packages" |
+1>/dev/null 2>&1 pacman -S --needed --noconfirm -
+
+# Finish
+echo "[FINISHED]: xfce-packages installation in background."
+exit 0
