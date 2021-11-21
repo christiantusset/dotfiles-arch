cbc05dafb3d0ad2cceaa613f31c1816c1387f984
Christian Tusset <christiantusset@gmail.com>
2021-11-21 01:45:13 -0300

    update scripts

---
+++ scripts/others/git-clone.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+package=$1
+
+if [ ! -d ~/Downloads/git-downloads ]; then
+  mkdir ~/Downloads/git-downloads
+fi
+
+cd ~/Downloads/git-downloads
+git clone "$package"
+cd "$(basename "$package" .git)"
