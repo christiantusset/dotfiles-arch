c629b12a71484d3ad49106dcd248ddcabd53e204
Christian Tusset <christiantusset@gmail.com>
2021-11-21 01:12:19 -0300

    update scripts

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 option=$1
 
 case "$option" in
-    "all" )     sudo sh ./scripts/install/general-pacman-packages.sh && sh ./scripts/install/general-aur-packages.sh && sh ./scripts/install/interface-xfce-packages.sh ;;
-    "pacman" )  sudo sh ./scripts/install/general-pacman-packages.sh ;;
+    "all" )     sh ./scripts/install/general-pacman-packages.sh && sh ./scripts/install/general-aur-packages.sh && sh ./scripts/install/interface-xfce-packages.sh ;;
+    "pacman" )  sh ./scripts/install/general-pacman-packages.sh ;;
     "aur" )     sh ./scripts/install/general-aur-packages.sh ;;
     "xfce" )    sh ./scripts/install/interface-xfce-packages.sh ;;
 
