1a2bd2d6cf2fe953cd7f3cc0b8968da7194727eb
Christian Tusset <christiantusset@gmail.com>
2021-11-21 01:09:21 -0300

    update scripts

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 option=$1
 
 case "$option" in
-    "all" )     sudo sh ./scripts/install/general-pacman-packages.sh && sh ./scripts/install/general-aur-packages.sh && sudo sh ./scripts/install/interface-xfce-packages.sh ;;
+    "all" )     sudo sh ./scripts/install/general-pacman-packages.sh && sh ./scripts/install/general-aur-packages.sh && sh ./scripts/install/interface-xfce-packages.sh ;;
     "pacman" )  sudo sh ./scripts/install/general-pacman-packages.sh ;;
     "aur" )     sh ./scripts/install/general-aur-packages.sh ;;
-    "xfce" )    sudo sh ./scripts/install/interface-xfce-packages.sh ;;
+    "xfce" )    sh ./scripts/install/interface-xfce-packages.sh ;;
 
     *) echo "[ERROR]: no install flag with name \"$option\" found" && exit 1 ;;
 esac
