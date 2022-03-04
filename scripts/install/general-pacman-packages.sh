b2c38477b6725b9ff61e9717ce50b9eb4061a76b
Christian Tusset <christian_tusset@hotmail.com>
2022-03-04 11:42:54 -0300

    Update general-pacman-packages.sh

--- scripts/install/general-pacman-packages.sh
+++ scripts/install/general-pacman-packages.sh
@@ -52,6 +52,9 @@ systemctl start docker.service /status/stop
 
 # Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 
 # Finish
 echo "[FINISHED]: general-packages"
