a452160b8944dec61e3b7b3da3857eb4957b6655
Christian Tusset <christian_tusset@hotmail.com>
2023-09-24 20:59:23 -0300

    Create mystartup.sh

---
+++ config-files/mystartup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+#
+#
+sudo systemctl restart vmtoolsd
+
+
+#sudo chmod +x ~/mystartup.sh
+#$ crontab -e
+#@reboot  /home/user/startup.sh
+
+#OR
+#ln -s /etc/init.d/start_my_app /etc/rc.d/
+
+#OR
+#/etc/init.d/your_app
